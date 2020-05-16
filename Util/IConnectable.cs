a6c58f66d98daac58b15062e1f33cd09eff22c1d
David M <PieKing1215@users.noreply.github.com>
2020-05-15 20:51:13 -0400

    Create IConnectable interface

---
+++ Util/IConnectable.cs
@@ -0,0 +1,18 @@
+﻿using Terraria;
+
+namespace MoreMechanisms {
+
+    public enum ConnectableType {
+        Input, Output
+    }
+
+    public interface IConnectable {
+
+        Item[] GetItems(ConnectableType type);
+
+        bool Accepts(Item item, ConnectableType type);
+
+        void TransferredItem(Item transferred, int index, ConnectableType type);
+    }
+    
+}
